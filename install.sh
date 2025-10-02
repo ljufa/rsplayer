1593a00fa0b1edee6967314a2847946a0f6ecd37
Dragan <dlj@pop-os>
2025-10-02 20:30:55 +0200

    fix installation script

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 set -e
 device_arch=$(arch)
-echo "Device architecture is:${device_arch}"
-
+if [ "$device_arch" = "x86_64" ]; then
+    device_arch="amd64"
+elif [ "$device_arch" = "aarch64" ]; then
+    device_arch="arm64"
+elif [ "$device_arch" = "armv7l" ] || [ "$device_arch" = "armv6l" ]; then
+    device_arch="armhf"
+fi
+echo "Using architecture:${device_arch}"
 deb_file_name="rsplayer_${device_arch}"
-URL=$(curl -s https://api.github.com/repos/ljufa/rsplayer/releases/latest | grep browser_download_url | grep "${deb_file_name}" | grep .deb | cut -d '"' -f 4)
+URL=$(curl -s https://api.github.com/repos/ljufa/rsplayer/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep "_${device_arch}.deb" | head -n 1)
 echo Downloading installation package from "$URL" ...
 curl -L -o "${deb_file_name}".deb  "$URL"
 sudo dpkg -i --force-overwrite "${deb_file_name}".deb
